31af2f75f60874f8663469f88a01547b89125df8
wael-boudissaa <jw_boudissa@esi.dz>
2024-05-15 12:25:00 +0100

    base de donne fixed 3 actualite done emails demande visite

--- routes/clubRoute.ts
+++ routes/clubRoute.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as clubController from '../controllers/clubController';
+import EventController from '../controllers/eventController';
 
 const router = express.Router();
-
+const evenement = new EventController();
 // router.post('/create', clubController.createClub);
 // router.get('/allclubs', clubController.getAllClubs);
 // // router.get('/activesclubs', clubController.getActivesClubs);
 // router.get('/:id', clubController.getClubById);
 // router.put('/update/:id', clubController.updateClub);
 // router.delete('/delete/:id', clubController.deleteClub);
-
 export default router;
